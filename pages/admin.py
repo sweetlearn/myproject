1b8bab251c36b0c7bdfca542c0f99f287e3725ff
Abdulwahed Mansour <info@sweetlearn.com>
2020-03-29 00:41:03 +0300

    all from me

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer, Generator, SubLine
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Generator)
+admin.site.register(SubLine)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,52 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Customer(models.Model):
+    employee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    number = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=15)
+    
+        
+    created_date = models.DateTimeField(default=timezone.now)
+    join_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+        
+        
+        
+class Generator(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    capacitie = models.IntegerField(blank=True, null=True)
+    working = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+        
+        
+        
+class SubLine(models.Model):
+    Generator = models.ForeignKey(Generator, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    capacitie = models.IntegerField(blank=True, null=True)
+    working = models.BooleanField(default=True)
+    
+    
+    def __str__(self):
+        return self.name
+        
+        
+        
+        
+        
+        
+        
